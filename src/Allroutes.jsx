69cda261b9d8a84defa377fd3d44589b7138fd81
kumarborate <kumarborate111@gmail.com>
2023-08-16 13:03:33 +0530

    change route login

--- src/Allroutes.jsx
+++ src/Allroutes.jsx
@@ -3,8 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Master from './website/layout/Master'
 import Shop_grid from './website/maincomponent/pages/Shop_grid';
 import Home from './website/maincomponent/Home';
-import Login from './website/maincomponent/pages/Login';
-import Register from './website/maincomponent/pages/Register';
+
 import Brands from './website/maincomponent/pages/Brands';
 import ContactUs from './website/maincomponent/pages/ContactUs';
 import AboutUs from './website/maincomponent/pages/AboutUs';
@@ -13,6 +12,8 @@ import './App.css';
 import SubCatView from './website/maincomponent/SubCatView';
 import CartDetails from './website/maincomponent/pages/catView/CartDetails';
 import All_ProdShop from './website/maincomponent/pages/All_ProdShop';
+import Login from './website/authentication/Login';
+import Register from './website/authentication/Register';
 
 
 
---
+++ src/website/authentication/Auth_user.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Auth_user = () => {
+  return (
+    <div>Auth_user</div>
+  )
+}
+
+export default Auth_user
